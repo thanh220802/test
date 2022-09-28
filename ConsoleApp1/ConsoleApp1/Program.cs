6ebf70dd94a86033a7fabace33a7982409cee5d9
your name <abc@gmail.com>
2022-09-28 21:57:42 +0700

    upload source code

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace HELOO
+{
+    class Vukhi
+    {
+      public  string name = "Ten vu khi";
+       public Vukhi()
+        {
+            Console.WriteLine("Khoi tao");
+        }
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Vukhi sungluc = new Vukhi();
+            Console.WriteLine(sungluc.name);
+        }
+    }
+}
---
+++ tap danh python visua code/tap danh python visua code/tap_danh_python_visua_code.py
@@ -0,0 +1,4 @@
+print("hello")
+a=10
+b=6
+a+b
